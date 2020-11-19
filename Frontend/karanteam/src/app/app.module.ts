12790f051dbd3bced8596d91e65e32e17b25920b
Szőke Viktor <viktor.szoke@schdesign.hu>
2020-11-19 14:31:15 +0100

    Init pages

--- Frontend/karanteam/src/app/app.module.ts
+++ Frontend/karanteam/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
