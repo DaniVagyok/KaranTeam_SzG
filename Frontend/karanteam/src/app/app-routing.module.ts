2c7a2dbec3b18a49e146cafe531ad101b73c2cdc
Szőke Viktor <viktor.szoke@schdesign.hu>
2020-11-20 19:10:30 +0100

    Routing works

--- Frontend/karanteam/src/app/app-routing.module.ts
+++ Frontend/karanteam/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'main', component: MainPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
