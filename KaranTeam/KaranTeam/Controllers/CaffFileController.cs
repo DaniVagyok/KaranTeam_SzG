e850da4cb61e396bf37b91c5b31c223f83cb479b
Pribelszki Levente <pribelszkilevi@gmail.com>
2020-11-24 21:09:24 +0100

    Fix FileController

--- KaranTeam/KaranTeam/Controllers/CaffFileController.cs
+++ KaranTeam/KaranTeam/Controllers/CaffFileController.cs
@@ -1,12 +1,8 @@
 ﻿using KaranTeam.Models;
 using KaranTeam.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace KaranTeam.Controllers
@@ -28,26 +24,17 @@ namespace KaranTeam.Controllers
         public async Task<IEnumerable<FileModel>> GetCaffFiles() => await CaffFileService.GetFileList();
 
         [HttpGet("{id}")]
-        public async Task<FileModel> GetFileById(int fileId) => await CaffFileService.GetFileById(fileId);
+        public async Task<FileModel> GetFileById(int id) => await CaffFileService.GetFileById(id);
 
         [HttpPut("{id}")]
         public async Task ModifyFile(FileModel modifiedFile) => await CaffFileService.ModifyFile(modifiedFile);
 
         [HttpDelete("{id}")]
-        public async Task DeleteFile(int fileId) => await CaffFileService.RemoveFileById(fileId);
+        public async Task Remove(int id) => await CaffFileService.RemoveFileById(id);
 
         // TODO: Change to form data
         [HttpPost]
         public async Task<NewFileModel> UploadFile(NewFileModel newFile) => await CaffFileService.UploadFile(newFile);
-
-        // https://stackoverflow.com/a/3605510
-        [HttpGet("{id}")]
-        public async Task<FileResult> DownloadCaffFile(int fileUri)
-        {
-            byte[] fileBytes = System.IO.File.ReadAllBytes($"{fileUri}");
-            string fileName = "cool.caff";
-            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
-        }
 
     }
 }
