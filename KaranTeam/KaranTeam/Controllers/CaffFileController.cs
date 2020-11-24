31ba477ef356dfb5493bd14528589767176c7545
Pribelszki Levente <pribelszkilevi@gmail.com>
2020-11-24 20:07:16 +0100

    Add methods to CaffFileController

--- KaranTeam/KaranTeam/Controllers/CaffFileController.cs
+++ KaranTeam/KaranTeam/Controllers/CaffFileController.cs
@@ -1,8 +1,11 @@
-﻿using KaranTeam.Services;
+﻿using KaranTeam.Models;
+using KaranTeam.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,9 +19,35 @@ namespace KaranTeam.Controllers
     public class CaffFileController : ControllerBase
     {
         public ICaffFileService CaffFileService { get; }
-        public CaffFileController(ICaffFileService service)
+        public CaffFileController(ICaffFileService caffFileService)
         {
-            CaffFileService = service;
+            CaffFileService = caffFileService;
         }
+
+        [HttpGet]
+        public async Task<IEnumerable<FileModel>> GetCaffFiles() => await CaffFileService.GetFileList();
+
+        [HttpGet("{id}")]
+        public async Task<FileModel> GetFileById(int fileId) => await CaffFileService.GetFileById(fileId);
+
+        [HttpPut("{id}")]
+        public async Task ModifyFile(FileModel modifiedFile) => await CaffFileService.ModifyFile(modifiedFile);
+
+        [HttpDelete("{id}")]
+        public async Task DeleteFile(int fileId) => await CaffFileService.RemoveFileById(fileId);
+
+        // TODO: Change to form data
+        [HttpPost]
+        public async Task<NewFileModel> UploadFile(NewFileModel newFile) => await CaffFileService.UploadFile(newFile);
+
+        // https://stackoverflow.com/a/3605510
+        [HttpGet("{id}")]
+        public async Task<FileResult> DownloadCaffFile(int fileUri)
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes($"{fileUri}");
+            string fileName = "cool.caff";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
     }
 }
