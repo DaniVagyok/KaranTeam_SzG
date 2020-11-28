c630d351cf1074fd92fe944d08014e8ed23d9aeb
Dániel Jójárt <jojartd@gmail.com>
2020-11-28 23:49:34 +0100

    Add comment adder, need fix

--- KaranTeam/KaranTeam/ClientApp/src/app/pages/details-page/details-page.component.ts
+++ KaranTeam/KaranTeam/ClientApp/src/app/pages/details-page/details-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MainPageService } from 'src/app/services/main-page.service';
+import { ProfileService } from 'src/app/services/profile.service';
 import { IShopItemModel } from 'src/app/shared/models/shop-item.model';
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'karanteam-details-page',
@@ -13,13 +15,17 @@ export class DetailsPageComponent implements OnInit {
   thumbnailUri: string;
   shopItemId: number;
   isLoading: boolean = true;
+  commentToAdd: string;
+  userData: User;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private service: MainPageService
+    private service: MainPageService,
+    private profileService: ProfileService
   ) { }
 
   ngOnInit(): void {
     this.shopItemId = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.profileService.getUserData().subscribe(res => this.userData = res);
     this.service.getShopItemById(this.shopItemId).subscribe(res => {
       this.shopItem = res;
       this.thumbnailUri = `api/caff/${this.shopItem.id}/thumbnail`;
@@ -30,6 +36,13 @@ export class DetailsPageComponent implements OnInit {
 
   downloadImage(): void {
     // TODO: implement
+  }
+
+  addComment(){
+    this.shopItem.fileComments.push({id: this.shopItem.fileComments.length+1, ownerName: this.userData.userName, content: this.commentToAdd, creationDate: new Date() })
+    this.commentToAdd='';
+    console.log(this.shopItem);
+    this.service.updateShopItem(this.shopItem);
   }
 
 }
