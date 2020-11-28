4ba064f7df037b66ff24dbbf1754ae0649fa9360
Dániel Jójárt <jojartd@gmail.com>
2020-11-28 15:20:24 +0100

    Add token interceptor service

--- KaranTeam/KaranTeam/ClientApp/src/app/services/token-interceptor.service.ts
+++ KaranTeam/KaranTeam/ClientApp/src/app/services/token-interceptor.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService {
+export class TokenInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(private injector: Injector) { }
+
+  intercept(req, next){
+    let authService = this.injector.get(AuthService)
+    let tokenizedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${authService.getToken()}`
+      }
+    })
+    return next.handle(tokenizedReq)
+  }
 }
