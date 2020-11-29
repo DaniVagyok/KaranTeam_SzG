ba4eea62fb99cae0f199692924302f319e7fbbda
Szőke Viktor <viktor.szoke@schdesign.hu>
2020-11-29 20:00:59 +0100

    Download file

--- KaranTeam/KaranTeam/ClientApp/src/app/shared/components/shop-item/shop-item.component.ts
+++ KaranTeam/KaranTeam/ClientApp/src/app/shared/components/shop-item/shop-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { from, fromEventPattern } from 'rxjs';
+import { MainPageService } from 'src/app/services/main-page.service';
 import { IShopItemModel } from '../../models/shop-item.model';
 
 @Component({
@@ -10,13 +11,25 @@ import { IShopItemModel } from '../../models/shop-item.model';
 export class ShopItemComponent implements OnInit {
   @Input() shopItem: IShopItemModel;
   thumbnailUri: string;
-  constructor() { }
+  constructor(
+    private service: MainPageService,
+  ) { }
 
   ngOnInit(): void {
     this.thumbnailUri = `api/caff/${this.shopItem.id}/thumbnail`;
   }
 
   downloadImage(): void {
-    // TODO: implement
+    this.service.downloadCiffCaffFile(this.shopItem.id).subscribe(res => {
+      let url = window.URL.createObjectURL(res);
+      let a = document.createElement('a');
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.href = url;
+      a.download = this.shopItem.title + '.caff';
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    });
   }
 }
